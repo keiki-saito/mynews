11c0a7fd5a75163c1b644d5cfd634374e552d07e
EC2 Default User <ec2-user@ip-172-31-43-113.us-east-2.compute.internal>
2021-01-22 13:55:59 +0000

    修正

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,7 +11,7 @@
                             <div class="caption mx-auto">
                                 <div class="image">
                                     @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                       <img src="{{ $headline->image_path }}">
                                     @endif
                                 </div>
                                 <div class="title p-2">
@@ -43,17 +43,17 @@
                                     {{ str_limit($post->body, 1500) }}
                                 </div>
                             </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
-                            </div>
+                           <div class="image col-md-6 text-right mt-4">
+                             @if ($post->image_path)
+                               <img src="{{ $post->image_path }}">
+                             @endif
+                           </div>
                         </div>
                     </div>
                     <hr color="#c0c0c0">
                 @endforeach
             </div>
         </div>
-    </div>
+    </div>          
     </div>
 @endsection
